2d887431a45de9c624e8f8c963a613809601505a
script-js <134544514+script-js@users.noreply.github.com>
2024-03-13 15:49:47 -0700

    Update appinst.js

--- appinst.js
+++ appinst.js
@@ -16,6 +16,24 @@ function addapp(aurl,aatitle,imgurl,ico_appid,svg) {
   console.log("DebugInfo:" + aatitle + imgurl + aurl + " | " + toadd + " | " + ta2)
 }
 
+function addPWA(aurl,aatitle,imgurl,ico_appid,svg) {
+  if (ico_appid) {
+    var toaddpre = "new WIN('(link)[" + aurl + "]', '(icn)[" + imgurl + "]', '(title)[" + aatitle + "]', '(os)[false]', '(full)[false]', '(appname)[" + ico_appid + "]');";
+  } else {
+    var toaddpre = "new WIN('(link)[" + aurl + "]', '(icn)[" + imgurl + "]', '(title)[" + aatitle + "]', '(os)[false]', '(full)[false]', '(appname)[" + aatitle + "]');";    
+  }
+  if (svg) {
+    var toadd = localStorage.getItem("capps") + '<div class="deskapp" title="' + aatitle + '" onclick="' + toaddpre + '" xmlns="http://www.w3.org/2000/svg">' + svg + '</div>'
+ } else {
+    var toadd = localStorage.getItem("capps") + '<div class="deskapp" title="' + aatitle + '" onclick="' + toaddpre + '" xmlns="http://www.w3.org/2000/svg"><img width="38px" src=' + imgurl + '></div>'
+ }
+  var ta2 = localStorage.getItem("appremlist") + "<div name='" + aatitle + "' style='width:100%'><img style='float:left' width='38' src='" + imgurl + "' />&nbsp;<span style='float:left'>" + aatitle + "</span><button class='btn'  id='" + aatitle + "' onclick='removeApp(this.id)' style='float:right'>Uninstall</button></div><p>&nbsp;</p>"
+  localStorage.setItem("capps",toadd.replace("null",""))
+  localStorage.setItem("appremlist",ta2.replace("null",""))
+  alert("The app has been installed. Restart Terbium (Menu > Power Button) to see changes.")
+  console.log("DebugInfo:" + aatitle + imgurl + aurl + " | " + toadd + " | " + ta2)
+}
+
 function removeApp(appid) {
         var apptext = document.getElementById(appid).parentElement.innerHTML;
         var acon2 = confirm("Are you sure you want to uninstall this app?")
